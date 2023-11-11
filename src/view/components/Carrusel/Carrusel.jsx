568848eb0e97c6bcb5b00ade310314674ebfb2e9
Junior Arroyave <juniorarroyave2@gmail.com>
2023-11-11 18:00:27 -0500

    intento 2 error de imagenes al desplegar

--- src/view/components/Carrusel/Carrusel.jsx
+++ src/view/components/Carrusel/Carrusel.jsx
@@ -1,10 +1,4 @@
 import './Carrusel.css'
-import img1 from './img/1.png'
-import img2 from './img/2.png'
-import img3 from './img/3.png'
-import img4 from './img/4.png'
-import img5 from './img/5.png'
-import img6 from './img/6.png'
 
 export const Carrusel = () => {
   return (
@@ -16,42 +10,42 @@ export const Carrusel = () => {
               <h1>¡Asi de facil es usar VirtualSchedule!</h1>
               <p>Dirigete al apartado de registarme</p>
             </div>
-            <img src={img1} alt='' />
+            <div className='img' />
           </li>
           <li>
             <div className='carrusel__text'>
               <h1>¡Crea Tu usuario!</h1>
               <p>Ingresa tus datos personales</p>
             </div>
-            <img src={img2} alt='' />
+            <div className='img2' />
           </li>
           <li>
             <div className='carrusel__text'>
               <h1>¡Inicia sesión!</h1>
               <p>Coloca el correo y contraseña previamente creados</p>
             </div>
-            <img src={img3} alt='' />
+            <div className='img3' />
           </li>
           <li>
             <div className='carrusel__text'>
               <h1>¡Ya puedes empezar tu vida ordenada!</h1>
               <p>Crea tu primer tarea con Virtual Schedule</p>
             </div>
-            <img src={img4} alt='' />
+            <div className='img4' />
           </li>
           <li>
             <div className='carrusel__text'>
               <h1>¿Tareas pendientes?</h1>
               <p>Dale al boton verde cuando ya hayas completado tus tareas</p>
             </div>
-            <img src={img5} alt='' />
+            <div className='img5' />
           </li>
           <li>
             <div className='carrusel__text'>
               <h1>¡Felicidades!</h1>
               <p>Has completado todos los pasos disfruta de VS las veces que quieras :D</p>
             </div>
-            <img src={img6} alt='' />
+            <div className='img6' />
           </li>
         </ul>
       </div>
---
+++ src/view/components/Carrusel/img/fotos.jsx
@@ -0,0 +1,5 @@
+export const img1 = () => {
+  return (
+    <img src='./img/1.png' alt='' />
+  )
+}
